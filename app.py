88a10f263cba7ffa530edfb041a17eb8608735e5
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-03-12 23:56:45 -0400

    Add time entry

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.fields.html5 import DateField, TimeField
+from wtforms.validators import Email, Length, DataRequired, NumberRange, InputRequired, EqualTo
+
+import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super secret key'
+
+
+@app.before_request
+def before_request():
+    db.open_db_connection()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db.close_db_connection()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/time-sheet')
+def time_sheet():
+    return render_template('time-sheet.html', entries=db.read_time_entries())
+
+
+class TimeEntryForm(FlaskForm):
+    start_date = DateField('Start Date')
+    start_time = TimeField('Start Time')
+    end_date = DateField('End Date')
+    end_time = TimeField('End Time')
+    description = StringField('Description')
+    submit = SubmitField('Add Time Entry')
+
+
+@app.route('/time-entry', methods=['GET', 'POST'])
+def time_entry():
+    time_entry_form = TimeEntryForm()
+    if time_entry_form.validate_on_submit():
+        rowcount = db.create_time_entry(1, 1,
+                                        time_entry_form.start_date.data,
+                                        time_entry_form.start_time.data,
+                                        time_entry_form.end_date.data,
+                                        time_entry_form.end_time.data,
+                                        time_entry_form.description.data)
+        if rowcount == 1:
+            flash("Added time successfully")
+            return redirect(url_for('time_sheet'))
+        else:
+            flash("Problem adding time")
+    return render_template('time-entry.html', form=time_entry_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,33 @@
+from flask import g
+import psycopg2
+import psycopg2.extras
+
+data_source_name = "dbname=time-machine user=tom host=localhost"
+
+
+def open_db_connection():
+    g.connection = psycopg2.connect(data_source_name)
+    g.cursor = g.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def close_db_connection():
+    g.cursor.close()
+    g.connection.close()
+
+
+def read_time_entries():
+    g.cursor.execute('SELECT * FROM time ORDER BY start_date, start_time')
+    return g.cursor.fetchall()
+
+
+def create_time_entry(user_id, project_id, start_date, start_time, end_date, end_time, description):
+    query="""
+    INSERT INTO public.time (description, project_id, user_id, start_date, start_time, end_date, end_time)
+    VALUES (%(desc)s, %(pid)s, %(uid)s, %(sd)s, %(st)s, %(ed)s, %(et)s)
+    """
+    g.cursor.execute(query, {
+        'desc': description, 'pid': project_id, 'uid': user_id,
+        'sd': start_date, 'st': start_time, 'ed': end_date, 'et': end_time
+    })
+    g.connection.commit()
+    return g.cursor.rowcount
